b4fa902ef81ef537b7f3516fdffaa528b956f89e
Leopoldo Lira <lira.leopoldo97@gmail.com>
2024-06-27 20:12:49 -0600

    task: added funtions.php file to load custom style sheet

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+
+function theme_styles(){
+    wp_register_style('main_stylesheet', get_stylesheet_directory_uri(). '/assets/styles/css/main.min.css', '','1.0', 'all');
+    wp_enqueue_style('main_stylesheet');
+}
+
+add_action('wp_enqueue_scripts', 'theme_styles');
