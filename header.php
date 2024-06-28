ea73abe429fc9f10e47ae896bce48683053cb3b1
Leopoldo Lira <lira.leopoldo97@gmail.com>
2024-06-27 21:23:19 -0600

    task: added header styles

--- header.php
+++ header.php
@@ -8,3 +8,26 @@
 </head>
 <body <? body_class();?>>
     
+<header class='header-container'>
+    <div class='header-container__logo'>
+        <picture>
+            <? $LOGO = get_field('page_logo','option')?>
+            <img src='<?=$LOGO;?>' />
+        </picture>
+    </div>
+    <nav class='header-container__navigation'>
+        <? $ARGS = array('menu' => 'header_navigation',
+                         'container' => false,
+                         'depth' => 1);
+        wp_nav_menu($ARGS);
+                         ?>
+
+        <picture>
+            <? $SEARCH_ICON = get_field('search_icon','option');?>
+            <img src='<?=$SEARCH_ICON?>' />
+        </picture>
+    </nav>
+    <!-- <div class='header-container__search'>
+        
+    </div> -->
+</header>
