22153dc0ba7cafdaad7fd254d3d9f2c9184fc7fe
Leopoldo Lira <lira.leopoldo97@gmail.com>
2024-06-27 20:23:20 -0600

    feat: added Article Custom Post Type in Functions.php File

--- functions.php
+++ functions.php
@@ -9,3 +9,22 @@ function theme_styles(){
 add_action('wp_enqueue_scripts', 'theme_styles');
 
 add_theme_support('menus');
+
+function create_custom_post_type(){
+    register_post_type('articles',
+    array(
+        'labels' => array(
+            'name'=> __('Articles'),
+            'singular_name' =>__('Article')
+        ),
+        'public' => true,
+        'has_archive' => false,
+        'hierarchical' =>true,
+        'supports' => array('title','editor', 'page-attributes', 'thumbnail'),
+        'menu_icon' => 'dashicons-portfolio'
+    ));
+
+    flush_rewrite_rules (false);
+}
+
+add_action('init', 'create_custom_post_type');
