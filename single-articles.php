8141b7dc3f7ca4d2ec83f4a03e85480cb3d6c8ec
Leopoldo Lira <lira.leopoldo97@gmail.com>
2024-06-28 17:09:22 -0600

    task: added the single-articles.php template for each individual article custom post type

---
+++ single-articles.php
@@ -0,0 +1,23 @@
+<?
+    /*
+        Template Name: Articles Template
+    */
+?>
+
+
+
+<? get_header();?>
+
+
+<main style='text-align:center; padding: 5rem 15rem; font-family: Spartan; font-weight: 400; font-size: 18px; line-height:30px'>
+    
+<h1><?=the_title();?></h1>
+
+<p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil nemo iure eaque molestias rerum facere excepturi eius vero nisi reprehenderit assumenda ex ab nobis sint distinctio temporibus debitis, maiores natus, corporis vitae quia. Ad eum, qui fugit reprehenderit magnam nisi commodi quaerat? Dolor necessitatibus deleniti sint veniam numquam, velit voluptate sed minus eveniet laboriosam temporibus quibusdam molestias unde explicabo dicta ullam nobis eum nesciunt tempore officia nemo accusantium! Eligendi, necessitatibus recusandae, voluptates nihil culpa beatae harum quo doloremque repellendus aut repellat sit, aspernatur ratione. Sapiente, tenetur, sint quo eos aspernatur amet ab fugit repudiandae molestiae atque quos laborum! Maiores sed exercitationem, accusamus alias ducimus facere commodi debitis incidunt vero, eligendi illo natus minus tempora earum fuga? Et magni soluta aut illo expedita. Repudiandae corrupti sequi quia adipisci alias voluptatem. Placeat quod nobis iure dicta velit ea saepe reiciendis. Vel quam totam fugiat eos, iusto mollitia omnis ipsa laborum sunt. Distinctio ab ullam a voluptatum inventore ducimus ipsum soluta nemo sint blanditiis. Est nisi error maiores, perferendis officia amet autem minima placeat officiis optio quos magnam earum doloribus dignissimos adipisci, libero, enim exercitationem natus delectus necessitatibus molestias. Vel numquam architecto dicta recusandae voluptate voluptatum ex dolores, earum ipsa placeat repellendus, iusto illum nesciunt, expedita magni velit. Nihil unde iusto ab perferendis aliquid! Aspernatur atque voluptatem vel hic omnis saepe fugit adipisci. Quibusdam nisi dicta dignissimos facere eum voluptatum modi cum necessitatibus. Minima, cumque corporis nisi iure officia rem illo nostrum corrupti hic inventore iusto itaque qui vero repellendus dolorum libero, aspernatur voluptas eaque odit laborum optio? Omnis odit maxime dolores temporibus numquam vel culpa laudantium quas nulla, laborum aperiam qui nobis, ducimus tempore aliquid autem eos? Voluptatem, adipisci inventore quam blanditiis quis ut harum optio modi? Corrupti placeat est, aut laudantium expedita ipsum tempore at unde minima illo, rerum quaerat. Excepturi nemo, sed recusandae inventore ipsam reprehenderit pariatur culpa alias eaque, cupiditate reiciendis dignissimos ad libero consequatur? Minus quisquam quasi tempora at. Aperiam placeat, consectetur ea exercitationem voluptatum repellat at deserunt ipsa reiciendis cupiditate quasi ad a tenetur eos nam modi eligendi quos, rerum id nobis, praesentium perferendis. Nobis nam tempora doloremque esse fugiat dolorem omnis explicabo quibusdam illo inventore corrupti possimus atque distinctio minus, molestias perferendis numquam rerum quas tempore officiis praesentium modi officia eaque aperiam. Optio molestiae laboriosam in deserunt deleniti pariatur, dolore nulla quam commodi quibusdam? Iure neque libero, ad pariatur commodi sapiente deserunt beatae cum itaque, modi maxime quaerat laudantium porro repudiandae totam quibusdam nam? Quaerat cum at illum accusantium earum repellat ex quasi eveniet, mollitia exercitationem. Reprehenderit assumenda quod ipsa consequuntur ullam architecto, in, optio culpa laboriosam incidunt accusamus quaerat, suscipit perferendis. Explicabo illum exercitationem cupiditate placeat nostrum ipsa voluptate ratione ipsum nesciunt vero molestias, deleniti minus cum mollitia, ullam excepturi assumenda aperiam hic eaque quia. Amet atque itaque, odio iusto ex cum similique sunt quas at quos laborum. Repellat, officiis. Accusantium vitae eveniet, labore dicta iusto nihil. Aut tempore accusantium qui corrupti quaerat iure! Commodi quas mollitia dolor veniam expedita quae officia voluptatibus fugit pariatur.
+    </p>
+
+</main>
+
+
+<? get_footer();?>
