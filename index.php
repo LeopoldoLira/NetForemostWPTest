5c484c4ceac03e76b21201d32a2bafcf460f2b78
Leopoldo Lira <lira.leopoldo97@gmail.com>
2024-06-28 10:34:35 -0600

    task: added the hero section to the landing page.

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <? get_header();?>
 
+<!-- Hero Section -->
+<section class='hero-section'>
+    <div class='hero-section__text'>
+        <span>Analytics</span>
+
+        <h1>Many desktop Publishing <br/> packages and <br/>web page</h1>
+
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis eros sed risus sollicitudin fringilla dictum in metus. 
+        </p>
+    </div>
+    <div class='hero-section__image'>
+        <picture>
+            <?
+                $HERO_IMAGE = get_field('hero_image','option');
+                $HERO_IMAGE_ALT = get_field('hero_image_alt', 'option');
+            ?>
+            <img src='<?=$HERO_IMAGE;?>' alt='<?=$HERO_IMAGE_ALT?>'/>
+        </picture>
+    </div>
+
+</section>
+
+
+
 <? get_footer();?>
